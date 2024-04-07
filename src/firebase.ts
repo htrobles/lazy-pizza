c97cdd3eee5636832347efb616a540e21da07ed6
htrobles <hjt.robles@gmail.com>
2024-04-07 19:02:38 -0400

    include firebase config

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDUukVtWqAH1aEBv-xKzVhkTaO15E43vPE',
+  authDomain: 'lazy-pizza-973c7.firebaseapp.com',
+  projectId: 'lazy-pizza-973c7',
+  storageBucket: 'lazy-pizza-973c7.appspot.com',
+  messagingSenderId: '245468448246',
+  appId: '1:245468448246:web:b4c65a42acbbabcd4b33d7',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
