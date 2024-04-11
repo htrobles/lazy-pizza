f7fb8d6e47e17cee8b591f26ebca447bf49233b2
htrobles <hjt.robles@gmail.com>
2024-04-11 01:44:58 -0400

    include checkout

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,21 @@
-  
+import { CartItemType } from './components/pages/Checkout/CartItem';
 import { ECheese, EMeat, ESauce, EVeggie } from './enums';
 
+export interface Address {
+  line1: string;
+  line2: string;
+  city: string;
+  province: string;
+}
+
 export interface User {
-    email:string;
-    firstName?: string;
-    lastName?: string;
-    address1?: string;
-    address2?: string;
-    contact?: string;
-  }
+  email: string;
+  firstName?: string;
+  lastName?: string;
+  address1?: string;
+  address2?: string;
+  contact?: string;
+}
 
 export interface Ingredient<IngredientEnum> {
   key: IngredientEnum;
@@ -26,4 +33,14 @@ export interface MyAlertProps {
   open: boolean;
   alertType: 'success' | 'error' | 'warning' | 'info';
   message: string;
+}
+
+export interface Order {
+  email: string;
+  id: string;
+  items: CartItemType[];
+  total: number;
+  address: Address;
+  orderDate: string;
+  paymentType: string;
 }
