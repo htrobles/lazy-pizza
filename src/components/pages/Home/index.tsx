fed7a06e60972e914bc737f5634a9c646eb7dea0
htrobles <hjt.robles@gmail.com>
2024-06-24 19:19:45 -0400

    fix build button in home screen

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './Home.scss';
 import { Button } from '@mui/material';
+import { NavLink } from 'react-router-dom';
 
 export default function Home() {
   return (
     <div className='home'>
       <div className='copy'>
         <h1>We can't do everything for you</h1>
-        <a href='/build'>
+        <NavLink to='/build'>
           <Button variant='contained' size='large'>
             BUILD YOUR OWN PIZZA
           </Button>
-        </a>
+        </NavLink>
       </div>
       <img className='pizza' src='/pizza-top-view.png' alt='Delicious pizza' />
     </div>
